6d1e4982f68786219bee0551e68157cd977fbbd5
Bipul Neupane <83596423+bipul018@users.noreply.github.com>
2025-01-14 02:37:32 +0545

    only GOD understands the errors now
    Just removed the derive Defaults and defaulted to using own self made
    default implementations even though there is literally ZERO reason for
    it to fail as the types used are either have default null or are
    optional.
    
    Also fixed some borrowing/mutability errors, had to use a for loop
    like a caveman in one place.
    
    Fiddled with knobs and made them into references.
    
    Tried adding the marker field and still failed (I think because of
    that.)
    
    Now going to remove the marker field again and trying to see if it works

--- src/process_unit.rs
+++ src/process_unit.rs
@@ -10,8 +10,8 @@ use vulkanalia::prelude::v1_1::*;
 use vulkanalia::prelude::v1_2::*;
 use vulkanalia::prelude::v1_3::*;
 
-#[derive(Default)]
-pub struct FactoryObjectBase<'a, 'b>{
+//#[derive(Default)]
+pub struct FactoryObjectBase<'a, 'b, T>{
     desc_pool: vk::DescriptorPool,
     desc_set: vk::DescriptorSet,
     //Needed because at writing of command buffer, need to set barriers (TODO:: Insert mode for when array is copied also)
@@ -25,41 +25,54 @@ pub struct FactoryObjectBase<'a, 'b>{
     scalars: Vec<u8>,
 
     // Fk rust, i cant just have 'all other except this default'
-    factory: Option<&'a Factory<'a>>,
+    factory: Option<&'a Factory<'a, T>>,
     //ctx: Option<&'a Context>,
 }
 
-impl Drop for FactoryObjectBase<'_, '_>{
+impl<T> Drop for FactoryObjectBase<'_, '_, T>{
     fn drop(&mut self){
         self.clean();
     }
 }
 
-impl<'a,'b> FactoryObjectBase<'a,'b>{
+impl<T> Default for FactoryObjectBase<'_, '_, T>{
+    fn default() -> Self{
+        Self{
+            desc_pool: vk::DescriptorPool::null(),
+            desc_set: vk::DescriptorSet::null(),
+            inputs: vec![],
+            outputs: vec![],
+            scalars: vec![],
+            factory: None
+        }
+    }
+}
+
+impl<'a,'b, T> FactoryObjectBase<'a,'b, T>{
     pub fn ctx(&self) -> Option<&'a Context>{ self.factory?.ctx }
     pub fn clean(&mut self){
 	match self.ctx(){
 	    None => {},
 	    Some(ctx) => {
                 // Only cleanup if not null for each
-                self.outputs.map(|&arr|{
+                self.outputs.iter_mut().map(|arr|{
                     ctx.drop_array(arr);
-                    arr = DeviceF32Array::default();
+                    *arr = DeviceF32Array::default();
                 });
-                self.inputs.map(|&oarr|{
-                    oarr = (None, 0);
+                self.inputs.iter_mut().map(|oarr|{
+                    *oarr = (None, 0);
                 });
                 if self.desc_pool != vk::DescriptorPool::null(){
-                    ctx.dev.destroy_descriptor_pool(self.desc_pool);
+                    unsafe{ctx.dev.destroy_descriptor_pool(self.desc_pool, None)};
                     self.desc_pool = vk::DescriptorPool::null();
                 }
                 self.desc_set = vk::DescriptorSet::null();
 	    }
 	}
     }
-    pub fn new(factory: &'a Factory, input_sizes: &[u32], output_sizes: &[u32], scalar_count: usize) -> VkResult<Self>{
+    pub fn new(factory: &'a Factory<T>, input_sizes: &[u32], output_sizes: &[u32], scalar_count: usize) -> VkResult<Self>{
         let mut this = Self::default();
-        this.factory = factory;
+        this.factory = Some(factory);
         //TODO:: Print a informative error
         let ctx = this.ctx().unwrap();
 
@@ -78,12 +91,12 @@ impl<'a,'b> FactoryObjectBase<'a,'b>{
 		descriptor_pool(this.desc_pool).
 		set_layouts(&[factory.desc_layout]))}?[0];
 
-        this.inputs = input_sizes.map(|sz|{ (None, sz as u32) });
+        this.inputs = input_sizes.into_iter().map(|sz|{ (None, *sz as u32) }).collect();
         // 4 byts each for u32 or f32 or i32
         this.scalars = vec![0; scalar_count * 4];
         // Use a push mechanism for allowing cleanup rather than map
         for (inx, sz) in output_sizes.into_iter().enumerate(){
-            let arr = ctx.new_array(sz as usize, false)?;
+            let arr = ctx.new_array(*sz as usize, false)?;
             this.outputs.push(arr);
             unsafe{ctx.dev.update_descriptor_sets
             (&[vk::WriteDescriptorSet::builder().
@@ -93,7 +106,7 @@ impl<'a,'b> FactoryObjectBase<'a,'b>{
                 descriptor_type(vk::DescriptorType::STORAGE_BUFFER).
                 buffer_info(&[vk::DescriptorBufferInfo::builder().
                     offset(0).
-                    range(arr.size).
+                    range(arr.size as u64).
                     buffer(arr.buffer)])],
                 &([] as [vk::CopyDescriptorSet; 0]))};
         }
@@ -103,11 +116,11 @@ impl<'a,'b> FactoryObjectBase<'a,'b>{
     pub fn write_input(&mut self, inx: usize, arr_ref: &'b DeviceF32Array){
         let ctx = self.ctx().unwrap(); // Should we signal error ??
         // Zero, assert that the length supplied in is matching
-        assert_eq!(arr_ref.len() as u32, self.inputs[inx].1,
+        assert_eq!(arr_ref.count as u32, self.inputs[inx].1,
             "Length of supplied array is different from the expected value of length given at construction");
         
         // First write to the array list
-        self.inputs[inx].0 = arr_ref;
+        self.inputs[inx].0 = Some(arr_ref);
         // Then write to descriptor assuming initialized
         unsafe{ctx.dev.update_descriptor_sets
             (&[vk::WriteDescriptorSet::builder().
@@ -117,7 +130,7 @@ impl<'a,'b> FactoryObjectBase<'a,'b>{
                 descriptor_type(vk::DescriptorType::STORAGE_BUFFER).
                 buffer_info(&[vk::DescriptorBufferInfo::builder().
                     offset(0).
-                    range(arr_ref.size).
+                    range(arr_ref.size as u64).
                     buffer(arr_ref.buffer)])],
                 &([] as [vk::CopyDescriptorSet; 0]))};        
     }
@@ -139,21 +152,23 @@ impl<'a,'b> FactoryObjectBase<'a,'b>{
     // To make it able to work with multiple dispatches at once 
     pub fn setup_pre_cmd(&self, cmd_buf: &vk::CommandBuffer) {
         // TODO:: Make it return error instead of asserting
-        let fact = match self.fact{
-            None => assert!(false, "Need to have been produced from a factory. Homemade goods are not upto standard"),
-            Some(fact) => fact
-        };
+        //Again i can never have nice things in rust without too much worthless effort [but here i am toiling away anyway]
+        //let fact = match self.factory{
+        //    None => assert!(false, "Need to have been produced from a factory. Homemade goods are not upto standard"),
+        //    Some(fact) => fact
+        //};
+        let fact = self.factory.unwrap();
         let ctx = fact.ctx.unwrap(); // Should we signal error ??
 
         // Write the barriers
         // TODO:: Later allow to use a FactoryObject kind / copy operation kind, etc to distinguish between buffer that doesnot need any barrier, vs buffer that needs barriers against copy operation, vs buffer that needs barriers against write operation
-        let buff_barrs:Vec<vk::BufferMemoryBarrier2Builder> = 
-            self.inputs.map(|(x,_)| {
-                let arr_ref = match x{
-                    None => assert!(x != None, "All inputs must have been set before executing command"),
-                    Some(a) => a,
-                };
-                vk::BufferMemoryBarrier2::builder().
+        let mut buff_barrs:Vec<vk::BufferMemoryBarrier2Builder> = 
+            vec![vk::BufferMemoryBarrier2::builder(); self.inputs.len()];
+        for inx in 0..self.inputs.len(){
+            let (x,_) = self.inputs[inx];
+            buff_barrs[inx] = match x{
+                None => panic!("All inputs must have been set before executing command"),
+                Some(arr_ref) => vk::BufferMemoryBarrier2::builder().
                     src_stage_mask(vk::PipelineStageFlags2::COMPUTE_SHADER).
                     dst_stage_mask(vk::PipelineStageFlags2::COMPUTE_SHADER).
                     src_access_mask(vk::AccessFlags2::SHADER_STORAGE_WRITE).
@@ -162,18 +177,19 @@ impl<'a,'b> FactoryObjectBase<'a,'b>{
                     dst_queue_family_index(ctx.comp_fam).
                     buffer(arr_ref.buffer).
                     offset(0).
-                    size(arr_ref.size)
-            });
+                    size(arr_ref.size as u64)
+            };
+        }
         unsafe{ctx.dev.cmd_pipeline_barrier2(
-            cmd_buf,
+            *cmd_buf,
             &vk::DependencyInfo::builder().
                 dependency_flags(vk::DependencyFlags::empty()).
                 buffer_memory_barriers(&buff_barrs)
         )};
         unsafe{ctx.dev.cmd_push_constants
-                (cmd_buf, fact.pipe_layout, vk::ShaderStageFlags::COMPUTE, 0, &self.scalars)};
+                (*cmd_buf, fact.pipe_layout, vk::ShaderStageFlags::COMPUTE, 0, &self.scalars)};
 	unsafe{ctx.dev.cmd_bind_pipeline
-	       (cmd_buf, vk::PipelineBindPoint::COMPUTE, fact.pipeline)};
+	       (*cmd_buf, vk::PipelineBindPoint::COMPUTE, fact.pipeline)};
     }
 }
 
@@ -188,21 +204,19 @@ pub trait FactoryObject{
 
     // A static fxn that takes in the 'knob' struct and produces valuable information
     type Knobs;
-    // Becauze fking rust is a fking dumb little shit
-    type Array<N> = [u32;N];
-    fn input_array_sizes(knobs: Self::Knobs) ->
-Self::Array<Self::INPUT_ARRAY_COUNT>;
+    // Becauze fking rust is a fking dumb whiny little shit, i just cant return a nice static array whose sie can be determined at compile time just up ^^ there, now i have to use asserts like a common dynamic language
+    fn input_array_sizes(knobs: &Self::Knobs) -> Vec<u32>;
 //[u32;Self::INPUT_ARRAY_COUNT];
-    fn output_array_sizes(knobs: Self::Knobs) ->
-Self::Array<Self::OUTPUT_ARRAY_COUNT>;
+    fn output_array_sizes(knobs: &Self::Knobs) -> Vec<u32>;
 //[u32;Self::OUTPUT_ARRAY_COUNT];
 
     // Generate factory using the generic parameter as self
     // Provides the shader itself along with anything needed
     // This line happened because of fking rust. I cannot (in ways that a normal user should be able to do) ensure that Factory made with a type in mind only be used for that type. Fking have to use unsafe rust now. I swear after this basic version is completed i am quite likely quitting rust for good no matter if even god tells me to.
     //fn factory<'a>(ctx: &'a Context) -> VkResult<Factory<'a, Self>>;
-    fn factory<'a>(ctx: &'a Context) -> VkResult<Factory<'a>>;
-    fn new(base_obj: FactoryObjectBase, knobs: Self::Knobs) -> VkResult<Self> where Self: Sized;
+    fn factory<'a>(ctx: &'a Context) -> VkResult<Factory<'a,Self>>;
+    // Just remember, the stupid crab tells me just a reverse thing and tells me to 'relax the type from being sized at all'
+    fn new(base_obj: FactoryObjectBase<Self>, knobs: &Self::Knobs) -> VkResult<Self>;
 
     // Usage fxns
     // TODO:: Maybe it will be nicer to have a single fxn that takes in all needed arguments through another struct (like the Knobs) where one specifies all the necessary input arguments (like compulsory named parameters) and a command buffer and then it does everything like setting the arguments and calling setup_pre_cmd for the base object and then calling the dispatch. But for now will just have separate fxn that does everything separately ??
@@ -211,22 +225,34 @@ Self::Array<Self::OUTPUT_ARRAY_COUNT>;
 }
 
 
-pub struct Factory<'a>{
+//#[derive(Default)]
+pub struct Factory<'a, T>{
     pub desc_layout: vk::DescriptorSetLayout,
     pub pipe_layout: vk::PipelineLayout,
     pub pipeline: vk::Pipeline,
-
+    
     // F*CK RUST, FU*K RUST, FUC* RUST, *UCK RUST
     // If I try to uncomment this below line, then i have to literally litter the whole code base with additional generic type T and still it won't compile at a point telling me nonsense "it cannot know the size of type at compile time" like wtf is the use of std::marker::PhantomData then. I just wanted to make sure that this factory object wouldnot be misused by intitializing for a type and producing for another type
-    //_marker_for_assoc_type_of_product: std::marker::PhantomData<T>,
+    _marker_for_assoc_type_of_product: std::marker::PhantomData<T>,
     ctx: Option<&'a Context>,
 }
-impl Drop for Factory<'_>{
+
+impl<T> Default for Factory<'_,T>{
+    fn default() -> Self{
+        Self{
+            desc_layout: vk::DescriptorSetLayout::null(),
+            pipe_layout: vk::PipelineLayout::null(),
+            pipeline: vk::Pipeline::null(),
+            ctx: None,
+        }
+    }
+}
+impl<T> Drop for Factory<'_,T>{
     fn drop(&mut self){
         self.clean();
     }
 }
-impl<'a, T> Factory<'a> where T: FactoryObject{
+impl<'a, T> Factory<'a,T> where T: FactoryObject{
     pub fn clean(&mut self){
         match self.ctx{
 	    None => {},
@@ -234,11 +260,11 @@ impl<'a, T> Factory<'a> where T: FactoryObject{
                 // Only cleanup if not null for each
                 if self.pipeline != vk::Pipeline::null(){
                     unsafe{ctx.dev.destroy_pipeline(self.pipeline, None)};
-                    self.pipeline = None;
+                    self.pipeline = vk::Pipeline::null();
                 }
                 if self.pipe_layout != vk::PipelineLayout::null(){
                     unsafe{ctx.dev.destroy_pipeline_layout(self.pipe_layout, None)};
-                    self.pipe_layout = vk::DescriptorSetLayout::null();
+                    self.pipe_layout = vk::PipelineLayout::null();
                 }
                 if self.desc_layout != vk::DescriptorSetLayout::null(){
                     unsafe{ctx.dev.destroy_descriptor_set_layout(self.desc_layout, None)};
@@ -248,14 +274,14 @@ impl<'a, T> Factory<'a> where T: FactoryObject{
 	}
     }
 
-    pub fn new(ctx: &'a Context, shader_code: &Bytecode) -> VkResult<Factory<'a>>{
+    pub fn new(ctx: &'a Context, shader_code: &Bytecode) -> VkResult<Factory<'a,T>>{
 
         let mut this = Self::default();
 	this.ctx = Some(ctx);
 
         let desc_bindings:Vec<vk::DescriptorSetLayoutBindingBuilder> = (0..(T::INPUT_ARRAY_COUNT+T::OUTPUT_ARRAY_COUNT)).map(|i|{
             vk::DescriptorSetLayoutBinding::builder().
-                binding(i).
+                binding(i as u32).
                 descriptor_type(vk::DescriptorType::STORAGE_BUFFER).
                 descriptor_count(1).
                 stage_flags(vk::ShaderStageFlags::COMPUTE)
@@ -308,15 +334,15 @@ impl<'a, T> Factory<'a> where T: FactoryObject{
     }
 
     // Will also take additional struct type that is directly forwarded to type T as arguments
-    pub fn produce(&self, knobs: T::Knobs) -> VkResult<T> {
+    pub fn produce(&self, knobs: &T::Knobs) -> VkResult<T> {
         // Assert that we do have context
         let ctx = match self.ctx{
-            None => assert!(false, "Need to have context to produce anything"),
+            None => panic!("Need to have context to produce anything"),
             Some(c) => c,
         };
         
         // first make the factory object base
-        let base_obj = FactoryObjectBase::new(ctx, self.desc_layout, &T::input_array_sizes(knobs), &T::output_array_sizes(knobs), T::INPUT_SCALAR_COUNT)?;
+        let base_obj = FactoryObjectBase::new(self, &T::input_array_sizes(knobs)[0..], &T::output_array_sizes(knobs)[0..], T::INPUT_SCALAR_COUNT)?;
         // Then pass it into the factory object's new function
         return T::new(base_obj, knobs);
     }
